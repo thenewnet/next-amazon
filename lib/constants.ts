f4d06029623c95a67afc69cc319262be7820740f
TrungBQ <trungbq86@gmail.com>
2025-02-04 12:07:40 +0700

    09-create-product-details-page

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,4 @@ export const APP_SLOGAN =
   process.env.NEXT_PUBLIC_APP_SOLOGAN || 'Spend less, enjoy more'
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Amazon by nextjs and MongoDB'
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
