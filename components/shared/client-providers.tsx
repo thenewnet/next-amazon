9cb395191ec8692750620e06e446959f75d0ef7c
TrungBQ <trungbq86@gmail.com>
2025-02-06 15:02:20 +0700

    26-add-theme-color

--- components/shared/client-providers.tsx
+++ components/shared/client-providers.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import useCartSidebar from '@/hooks/use-cart-sidebar'
 import CartSidebar from './cart-sidebar'
 import { Toaster } from '../ui/toaster'
+import { ThemeProvider } from './theme-provider'
 
 export default function ClientProviders({
   children,
@@ -13,15 +14,17 @@ export default function ClientProviders({
 
   return (
     <>
-      {isCartSidebarOpen ? (
-        <div className='flex min-h-screen'>
-          <div className='flex-1 overflow-hidden'>{children}</div>
-          <CartSidebar />
-        </div>
-      ) : (
-        <div>{children}</div>
-      )}
-      <Toaster />
+      <ThemeProvider attribute='class' defaultTheme='system'>
+        {isCartSidebarOpen ? (
+          <div className='flex min-h-screen'>
+            <div className='flex-1 overflow-hidden'>{children}</div>
+            <CartSidebar />
+          </div>
+        ) : (
+          <div>{children}</div>
+        )}
+        <Toaster />
+      </ThemeProvider>
     </>
   )
 }
